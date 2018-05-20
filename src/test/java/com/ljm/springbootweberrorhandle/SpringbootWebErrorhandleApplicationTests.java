f05ae6c83a42f59fd399e909885b7b5f7da0c6b7
lijiaming <lijiaming1984@gmail.com>
2018-05-20 20:13:52 +0800

    init

---
+++ src/test/java/com/ljm/springbootweberrorhandle/SpringbootWebErrorhandleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ljm.springbootweberrorhandle;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootWebErrorhandleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
